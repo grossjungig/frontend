4ef2ff326f1f8c07a016d3a31d72e21db40dcd7e
akups <appiah-lindberg@outlook.com>
2021-01-02 21:48:38 +0100

    build

---
+++ build/precache-manifest.452b4e6195f3ecf0ce558c07c967bbdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04cdb16d18c519c12b57626d02019657",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6394f7e40daa9ed48e2",
+    "url": "/static/css/main.f04bbd4f.chunk.css"
+  },
+  {
+    "revision": "b15ffad6532c3d5a0540",
+    "url": "/static/js/2.05727b99.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.05727b99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6394f7e40daa9ed48e2",
+    "url": "/static/js/main.40a478a0.chunk.js"
+  },
+  {
+    "revision": "d4f7d70172481223be70",
+    "url": "/static/js/runtime-main.55011264.js"
+  },
+  {
+    "revision": "08b393b3705fd24bbfa3ef2c1bf0d973",
+    "url": "/static/media/header_image.08b393b3.png"
+  }
+]);
