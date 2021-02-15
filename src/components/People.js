08f5ddc724b5f8e8feee09ad3151b9763d01e75d
nidanurgunay <nidanur@sabanciuniv.edu>
2021-02-15 14:46:36 +0300

    alt added to img on people file

--- src/components/People.js
+++ src/components/People.js
@@ -28,7 +28,7 @@ export default class People extends Component {
           <div style={{ display: "flex", justifyContent: "center" }}>
             <div className="card_people">
               <div className="card_img">
-                <img src={img} style={{ width: "100%" }} />
+                <img src={img} style={{ width: "100%" }} alt="people" />
               </div>
               <div class="container_people">
                 <h4><b>{el.name}, {el.age}</b></h4>
