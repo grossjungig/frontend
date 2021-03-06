61ebbec0be91cba78391f1d8e56a5ef9d6accea4
Heisenberg <nancologist@gmail.com>
2021-03-06 20:08:56 +0100

    #Partners: Add locale.

--- src/pages/home/components/Partners/Partners.js
+++ src/pages/home/components/Partners/Partners.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Partners.css';
 import { partners } from './data';
+import homeLocales from '../../../../locales/locales.home.json';
+
+const { title } = homeLocales.partners;
 
 const Partners = () => {
+    const lang = localStorage.getItem('lang');
     return (
         <div className="partner-panel">
-            <h2 className="partner-panel__title">Partner</h2>
+            <h2 className="partner-panel__title">{title[lang]}</h2>
             <div className="partner-panel__items">{ partners.map((p) => (
                 <div className="partner" key={p.name} >
                     <a href={p.link} target="_blank" rel="noreferrer" >
