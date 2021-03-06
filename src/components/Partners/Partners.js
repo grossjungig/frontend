a3e5b4c04f12f8f8f4a0db3937733b9698d5e43d
Heisenberg <nancologist@gmail.com>
2021-03-06 01:42:40 +0100

    Add new partner and fix styling.

--- src/components/Partners/Partners.js
+++ src/components/Partners/Partners.js
@@ -7,15 +7,16 @@ import eu from '../../assets/images/partners/eu.jpeg';
 import fub from '../../assets/images/partners/fub.jpeg';
 import gzentrale from '../../assets/images/partners/gzentrale.png';
 import profund from '../../assets/images/partners/profund.png';
+import senioren from '../../assets/images/partners/senioren.webp';
 
-const logos = [ eu, esf, fub, gzentrale, audalis, profund ];
+const logos = [ eu, esf, fub, profund, gzentrale, audalis, senioren ];
 
 const Partners = () => {
     return (
         <div className="partner-panel">
             <h2 className="partner-panel__title">Partners</h2>
             <div className="partner-panel__items">{ logos.map((logo, index) => (
-                <div className="partner" id={'p'+index} key={index} >
+                <div className="partner" key={index} >
                     <img src={logo} alt="partner logo"/>
                 </div>
             ))}</div>
