1ef34904e71d49074e2c6786f3e445d97b20cd11
Heisenberg <nancologist@gmail.com>
2021-03-06 18:55:16 +0100

    Fix typo.

--- src/components/Partners/Partners.js
+++ src/components/Partners/Partners.js
@@ -5,7 +5,7 @@ import { partners } from './data';
 const Partners = () => {
     return (
         <div className="partner-panel">
-            <h2 className="partner-panel__title">Partners</h2>
+            <h2 className="partner-panel__title">Partner</h2>
             <div className="partner-panel__items">{ partners.map((p) => (
                 <div className="partner" key={p.name} >
                     <a href={p.link} target="_blank" rel="noreferrer" >
