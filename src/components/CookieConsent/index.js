6d97979b3785aeda70dd0ac382edd22e8f7395eb
Morteza Jalilifar <nancologist@gmail.com>
2021-03-25 17:28:05 +0100

    Add cookie consent texts.

--- src/components/CookieConsent/index.js
+++ src/components/CookieConsent/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './index.css';
+import locales from '../../locales/locales.cookieconsent.json';
 
 const CookieConsent = (props) => {
+  const lang = localStorage.getItem("lang");
   return (
       <div className="cookie-consent">
-        <div className="cookie-consent__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate unde fuga modi temporibus maiores hic excepturi ipsam eveniet ducimus eum.</div>
-        <button className="cookie-consent__btn" onClick={props.clicked}>Got it!</button>
-      </div> 
+        <p className="cookie-consent__text">{locales.content[lang]}</p>
+        <button className="cookie-consent__btn" onClick={props.clicked}>
+          {locales.btnText[lang]}
+        </button>
+      </div>
   )
 };
 
