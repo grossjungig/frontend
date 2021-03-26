b427d7a7746c285744cfe7ebf3131207c3642d58
Jen <jennifer@studydrive.net>
2021-03-26 15:03:45 +0100

    feature/homepage-contact tidying up and streamlining

--- src/pages/home/components/Contact/Contact.js
+++ src/pages/home/components/Contact/Contact.js
@@ -19,66 +19,15 @@ class Contact extends Component {
           </div>
           <div className={'home-contact-card'}>
             <div className={'home-contact-icons-container'}>
-              <img className={'home-contact-card-image'} src={email} alt="community-logo"/>
+              <img className={'home-contact-card-image'} src={email} alt="contact-logo"/>
               <h4>info@grossjungig.de</h4>
             </div>
             <div className={'home-contact-icons-container'}>
-              <img className={'home-contact-card-image'} src={phone} alt="community-logo"/>
+              <img className={'home-contact-card-image'} src={phone} alt="contact-logo"/>
               <h4>+49 30 55231271</h4>
             </div>
           </div>
         </div>
-
-        {/*
-        <H2 id="contact" style={{ textAlign: "center" }}>{homeLocales.contact[lang]}</H2>
-        <ContactPanel >
-          <ContactInfo>
-            <H2 style={{ textAlign: "center" }}>
-              Großjungig AI <br />
-              {homeLocales.care[lang]}
-            </H2>
-            <H4 style={{ textAlign: "center" }}>
-              {homeLocales.contact_us[lang]}{" "}
-            </H4>
-          </ContactInfo>
-          <ContactRow>
-            <img
-              style={{ height: "59px", width: "59px", itemsAlign: "center" }}
-              src="../image/Email.png"
-              alt="email"
-            />
-            <H4>info@grossjungig.de</H4>
-            <img
-              style={{ height: "59px", width: "59px", itemsAlign: "center" }}
-              src="../image/phone.png"
-              alt="phone"
-            />
-            <H4>+49 30 55231271</H4>
-          </ContactRow>
-        </ContactPanel>*/}
-        {/*
-
-        <div className={"why-cards-scroll-container"}>
-          <div className={"why-cards-container"}>
-            <div className={"why-cards"}>
-              <div className={'card'}>
-                <div className={'card-title'}>{homeLocales.card_1_titel[lang]}</div>
-                <img className={'card-image'} src={community} alt="community-logo"/>
-                <div className={'card-text'}>{homeLocales.card_1[lang]}</div>
-              </div>
-              <div className={'card'}>
-                <div className={'card-title'}>{homeLocales.card_2_titel[lang]}</div>
-                <img className={'card-image'} src={savings} alt="savings-logo"/>
-                <div className={'card-text'}>{homeLocales.card_2[lang]}</div>
-              </div>
-              <div className={'card'}>
-                <div className={'card-title'}>{homeLocales.card_3_titel[lang]}</div>
-                <img className={'card-image'} src={clarity} alt="clarity-logo"/>
-                <div className={'card-text'}>{homeLocales.card_3[lang]}</div>
-              </div>
-            </div>
-          </div>
-        </div>*/}
       </div>
     )
   }
