3e6090864d41d6e21a7a13db49b7f1108b720166
Jen <jennifer@studydrive.net>
2021-04-05 13:40:49 +0200

    feature/home-social-links adds social media icons and adds design changes to borders and respsonsive styling

--- src/pages/home/components/Contact/Contact.js
+++ src/pages/home/components/Contact/Contact.js
@@ -3,6 +3,10 @@ import "./contact.css";
 import homeLocales from "../../../../locales/locales.home.json";
 import email from '../../../../assets/icons/email.svg'
 import phone from '../../../../assets/icons/phone.svg'
+import facebook from '../../../../assets/icons/facebook.svg'
+import youtube from '../../../../assets/icons/youtube.svg'
+import insta from '../../../../assets/icons/insta.svg'
+import linkedIn from '../../../../assets/icons/linkedIn.svg'
 
 class Contact extends Component {
   render() {
@@ -11,20 +15,29 @@ class Contact extends Component {
       <div id="home-page-contact">
         <h2>{homeLocales.contact[lang]}</h2>
         <div className={'home-contact-container'}>
-          <div className={'home-contact-card'}>
+          <div className={'home-contact-text-card'}>
             <div className={'home-contact-text-container'}>
               <h2>Großjungig AI <br/> {homeLocales.care[lang]}</h2>
               <h4>{homeLocales.contact_us[lang]}{" "}</h4>
             </div>
           </div>
-          <div className={'home-contact-card'}>
-            <div className={'home-contact-icons-container'}>
-              <img className={'home-contact-card-image'} src={email} alt="contact-logo"/>
-              <h4>info@grossjungig.de</h4>
+          <div className={'home-contact-icons-card'}>
+            <div className={'home-contact-icon-container'}>
+              <div className={'home-contact-icon'}>
+                <img src={email} alt="contact-logo"/>
+                <h4>info@grossjungig.de</h4>
+              </div>
+              <div className={'home-contact-icon'}>
+                <img src={phone} alt="contact-logo"/>
+                <h4>+49 30 55231271</h4>
+              </div>
             </div>
-            <div className={'home-contact-icons-container'}>
-              <img className={'home-contact-card-image'} src={phone} alt="contact-logo"/>
-              <h4>+49 30 55231271</h4>
+            <h4>{homeLocales.social[lang]}</h4>
+            <div className={'home-contact-icon-social-container'}>
+              <a href={"https://google.com"} target={'_blank'} rel={'noreferrer'}><img src={facebook} alt="facebook-logo"/></a>
+              <a href={"https://google.com"} target={'_blank'} rel={'noreferrer'}><img src={youtube} alt="youtube-logo"/></a>
+              <a href={"https://google.com"} target={'_blank'} rel={'noreferrer'}><img src={insta} alt="insta-logo"/></a>
+              <a href={"https://google.com"} target={'_blank'} rel={'noreferrer'}><img src={linkedIn} alt="linkedIn-logo"/></a>
             </div>
           </div>
         </div>
