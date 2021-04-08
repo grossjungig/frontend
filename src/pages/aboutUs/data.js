8ec949fe5a306ed89c4359c92e33e89eefa49305
Luca Dalmasso <lucadalmasso@gmail.com>
2021-04-08 10:30:39 +0200

    added four members, changed put Christian and Skander on the top line

--- src/pages/aboutUs/data.js
+++ src/pages/aboutUs/data.js
@@ -3,6 +3,7 @@ import aboutLocales from "../../locales/locales.aboutus.json";
 import anastasia from '../../assets/images/team/anastasia.jpg';
 import olga from '../../assets/images/team/olga.jpg';
 import menna from '../../assets/images/team/menna.jpg';
+import morteza from '../../assets/images/team/morteza.jpg';
 
 import luca from '../../assets/images/team/luca.png';
 import magda from '../../assets/images/team/magdalena.jpg';
@@ -15,7 +16,10 @@ import sree from '../../assets/images/team/sree.jpg';
 import tammy from '../../assets/images/team/tammy.jpg';
 import vicky from '../../assets/images/team/vicky.jpg';
 import vlad from '../../assets/images/team/vlad.jpg';
-import morteza from '../../assets/images/team/morteza.jpg';
+import christian from '../../assets/images/team/christian.png';
+import valentina from '../../assets/images/team/valentina.png';
+import jen from '../../assets/images/team/jen.png';
+import kilian from '../../assets/images/team/kilian.png';
 
 
 const { team } = aboutLocales;
@@ -36,19 +40,23 @@ export const coreStaff = [
     {
         img: morteza, role: team.Morteza, name: 'Morteza Jalilifar',
         link: 'https://www.linkedin.com/in/nancologist/'
-    }
+    },
 ]
 
 export const staff = [
-    { img: luca, role: team.Luca, name: 'Luca' },
-    { img: tammy, role: team.Tammy, name: 'Tammy' },
+    { img: skander, role: team.Skander, name: 'Skander' },
+    { img: christian, role: team.Christian, name: 'Christian' },
     { img: rosina, role: team.Rosina, name: 'Rosina' },
     { img: pablo, role: team.Pablo, name: 'Pablo' },
+    { img: tammy, role: team.Tammy, name: 'Tammy' },
+    { img: kilian, role: team.Kilian, name: 'Kilian' },
     { img: vicky, role: team.Vicky, name: 'Vicky' },
+    { img: jen, role: team.Jen, name: 'Jen' },
+    { img: valentina, role: team.Valentina, name: 'Valentina' },
+    { img: luca, role: team.Luca, name: 'Luca' },
     { img: magda, role: team.Magda, name: 'Magdalena' },
     { img: nida, role: team.Nida, name: 'Nida' },
     { img: maria, role: team.Maria, name: 'Maria' },
     { img: sree, role: team.Sree, name: 'Sree' },
-    { img: skander, role: team.Skander, name: 'Skander' },
     { img: vlad, role: team.Vlad, name: 'Vlad' }
 ];
