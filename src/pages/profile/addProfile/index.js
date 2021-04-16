d2ea91a1435e7a806ffffcc04ff58c5b24cb7e44
Heisenberg <nancologist@gmail.com>
2021-04-16 11:31:31 +0200

    Improve stylings

--- src/pages/profile/addProfile/index.js
+++ src/pages/profile/addProfile/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Redirect,Link } from "react-router-dom";
-import './index.css';
+import styles from './index.module.css';
+import { fullBlock } from '../../../shared/index.module.css';
 import axios from '../../../axios';
 import newAxios from 'axios';
 import { connect } from 'react-redux';
@@ -130,131 +131,140 @@ class AddProfile extends Component {
       return <Redirect to="/profiles" />;
     }
 
-
-
     return (
-
-      <div style={{ height: "auto", width: "auto" }}>
-        <div style={{ display: "flex", justifyContent: "center", marginTop: "3vh" }}>
-          <div className="warning" style={{ margin: "1vh" }}>
-            <p>Please do not leave your personal identifying information here.</p>
+      <div className={fullBlock}>
+        <div className={styles.main}>
+          <div className={styles.msg}>
+            Please do not leave your personal identifying information here.
           </div>
-        </div>
 
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <div style={{ width: "328px" }}>
-
-            <label className="label_profile" htmlFor="name" style={{marginBottom:"2vh"}}>First Name</label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              value={this.state.name}
-              onChange={this.setFormState}
-              className="input_profile"
-            />
-
-            <label className="label_profile" htmlFor="gender">Gender</label>
-            <select
-              name="gender"
-              type="select"
-              value={this.state.gender}
-              onChange={this.setFormState}
-              style={{ marginTop: "2vh" }}
-              className="select_profile"
-            >
-              <option  value="" disabled>Select</option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-              <option value="divers">Divers</option>
-            </select>
-
-            <label htmlFor="age" className="label_profile" style={{marginBottom:"2vh"}} >Age</label>
-            <input
-              type="text"
-              name="age"
-              id="age"
-              value={this.state.age}
-              onChange={this.setFormState}
-              className="input_profile"
-            />
-
-            <label className="label_profile" htmlFor="price" style={{marginBottom:"2vh"}}>Requested room price</label>
-            <input
-              type="number"
-              name="price"
-              id="price"
-              value={this.state.price}
-              onChange={this.setFormState}
-              className="input_profile"
-            />
-            <label htmlFor="district" className="label_profile" >About me (max 120 signs)</label>
-            <textarea
-              type="text"
-              name="description"
-              id="description"
-              value={this.state.description}
-              onChange={this.setFormState}
-              maxLength="120"
-              rows="3"
-              style={{ marginTop: "2vh" }}
-              className="textarea_profile"
-            />
-
-            <label className="label_profile" htmlFor="help" style={{marginBottom:"2vh"}}>Offered Help</label>
-            <Select isMulti options={options} onChange={this.setHelp} id="help"
-              name="help" />
-
-            <label className="label_profile" htmlFor="select">Prefered district</label>
-            <select
-              name="district"
-              type="select"
-              value={this.state.district}
-              onChange={this.setFormState}
-              className="select_profile"
-              placeholder="Select"
-            >
-              <option value="" disabled>Select</option>
-              <option value="Charlottenburg-Wilmersdorf">Charlottenburg-Wilmersdorf</option>
-              <option value="Friedrichshain-Kreuzberg">Friedrichshain-Kreuzberg</option>
-              <option value="Lichtenberg">Lichtenberg</option>
-              <option value="Marzahn-Hellersdorf">Marzahn-Hellersdorf</option>
-              <option value="Mitte">Mitte</option>
-              <option value="Neukoelln">Neukoelln</option>
-              <option value="Pankow">Pankow</option>
-              <option value="Reinickendorf">Reinickendorf</option>
-              <option value="Spandau">Spandau</option>
-              <option value="Steglitz-Zehlendorf">Steglitz-Zehlendorf</option>
-              <option value="Tempelhof-Schoeneberg">Tempelhof-Schoeneberg</option>
-              <option value="Treptow-Koepenick">Treptow-Koepenick</option>
-            </select>
-
-            <label className="label_profile" >Profile picture</label>
-            <img src={this.state.avatarPreview} className="avatar-preview" alt="avatar"/>
-            <input
-                type="file"
-                accept="image/png, image/jpeg, image/jpg"
-                onChange={this.handleAvatarChange}
-            />
-            <span className="avatar-preview-err">{this.state.avatarPreviewErr}</span>
-
-            <div className="warning" style={{ marginTop: "2vh" }}>
-              <p>By creating a request, you agree to our Terms and Conditions and Data Privacy Policy.</p>
-          
+          <div className={styles.form}>
+            <div className={styles.formCtrl}>
+              <label htmlFor="name">First Name</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                value={this.state.name}
+                onChange={this.setFormState}
+                className={styles.input}
+              />
             </div>
 
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <Link to={`/userportal`}>
-              <button type="submit" className="button_profile" style={{ width: "150px" }}>Cancel</button>
-              </Link>
-              <button type="submit" className="button_profile" style={{ width: "150px", background: "#365FA7", color: "#F9F8F8" }} onClick={this.addNewProfile} >Submit</button>
+            <div className={styles.formCtrl}>
+              <label htmlFor="gender">Gender</label>
+              <select
+                name="gender"
+                type="select"
+                value={this.state.gender}
+                onChange={this.setFormState}
+                className="select_profile"
+                className={styles.input}
+              >
+                <option  value="" disabled>Select</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="divers">Divers</option>
+              </select>
             </div>
-            {this.state.message && <p>{this.state.message}</p>}
 
-          </div >
-          <div>
+            <div className={styles.formCtrl}>
+              <label htmlFor="age">Age</label>
+              <input
+                type="text"
+                name="age"
+                id="age"
+                value={this.state.age}
+                onChange={this.setFormState}
+                className={styles.input}
+              />
+            </div>
 
+            <div className={styles.formCtrl}>
+              <label htmlFor="price">Requested room price</label>
+              <input
+                type="number"
+                name="price"
+                id="price"
+                value={this.state.price}
+                onChange={this.setFormState}
+                className={styles.input}
+              />
+            </div>
+
+            <div className={styles.formCtrl}>
+              <label htmlFor="district" >About me (max 120 signs)</label>
+              <textarea
+                type="text"
+                name="description"
+                id="description"
+                value={this.state.description}
+                onChange={this.setFormState}
+                maxLength="120"
+                rows="3"
+                className={styles.input}
+              />
+            </div>
+
+            <div className={styles.formCtrl}>
+              <label htmlFor="help">Offered Help</label>
+              <Select
+                isMulti
+                options={options}
+                onChange={this.setHelp}
+                id="help"
+                name="help"
+                className={styles.input}
+              />
+            </div>
+
+            <div className={styles.formCtrl}>
+              <label htmlFor="select">Prefered district</label>
+              <select
+                name="district"
+                type="select"
+                value={this.state.district}
+                onChange={this.setFormState}
+                placeholder="Select"
+                className={styles.input}
+              >
+                <option value="" disabled>Select</option>
+                <option value="Charlottenburg-Wilmersdorf">Charlottenburg-Wilmersdorf</option>
+                <option value="Friedrichshain-Kreuzberg">Friedrichshain-Kreuzberg</option>
+                <option value="Lichtenberg">Lichtenberg</option>
+                <option value="Marzahn-Hellersdorf">Marzahn-Hellersdorf</option>
+                <option value="Mitte">Mitte</option>
+                <option value="Neukoelln">Neukoelln</option>
+                <option value="Pankow">Pankow</option>
+                <option value="Reinickendorf">Reinickendorf</option>
+                <option value="Spandau">Spandau</option>
+                <option value="Steglitz-Zehlendorf">Steglitz-Zehlendorf</option>
+                <option value="Tempelhof-Schoeneberg">Tempelhof-Schoeneberg</option>
+                <option value="Treptow-Koepenick">Treptow-Koepenick</option>
+              </select>
+            </div>
+
+            <div className={styles.formCtrl}>
+              <label >Profile picture</label>
+              <img src={this.state.avatarPreview} className="avatar-preview" alt="avatar"/>
+              <input
+                  type="file"
+                  accept="image/png, image/jpeg, image/jpg"
+                  onChange={this.handleAvatarChange}
+                  className={styles.input}
+              />
+              <span className="avatar-preview-err">{this.state.avatarPreviewErr}</span>
+            </div>
+
+            <div className={styles.msg}>
+              By creating a request, you agree to our Terms and Conditions and Data Privacy Policy.
+            </div>
+
+            <div>
+              <button type="submit" className={styles.btn} onClick={this.addNewProfile} >Submit</button>
+            </div>
           </div>
+          {this.state.message && <p>{this.state.message}</p>}
         </div>
       </div>
     );
