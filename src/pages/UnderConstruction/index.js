f2fa182451716e66c10e58a986a8fc6bdde1a310
Heisenberg <nancologist@gmail.com>
2021-05-02 18:11:53 +0200

    Header Styling

--- src/pages/UnderConstruction/index.js
+++ src/pages/UnderConstruction/index.js
@@ -5,13 +5,13 @@ import bg from '../../assets/images/backgrounds/hero-home-background-picture.jpg
 
 export default () => {
     return (
-        <div className={styles.main}>
+        <div className={styles.cmp}>
             
             {/* Background */}
             <img src={bg} alt="background" className={styles.bg} />
             
             {/* Content */}
-            <div className={styles.content}>
+            <div className={styles.body}>
                 
                 {/* Header */}
                 <div className={styles.header}>
@@ -19,13 +19,25 @@ export default () => {
                     <div>Social Nets</div>
                 </div>
 
-                <Countdown />
+                {/* Content */}
+                <div className={styles.content}>
+                    <div className={styles.contentL}>
+                        <Countdown />
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, iure.</p>
+                    </div>
+                    <div className={styles.contentR}>
+                        <p>Newsletter</p>
+                        <p>Subscribe (Email, Name)</p>
+                        <button>subscribe</button>
+                    </div>
+                </div>
 
 
                 {/* Footer */}
                 <div className={styles.footer}>
-                    Copyright 2021 | About Us | Partnership
+                    Copyright 2021 | About Us | How It Works | Partnership
                 </div>
+
             </div>
 
         </div>
