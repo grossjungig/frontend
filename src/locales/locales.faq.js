69c5137a6b24c1262d443bb51eec2a64222ebdd3
Nidanur Gunay <nidanur@sabanciuniv.edu>
2021-06-09 13:13:21 +0300

    Added questions without endl

--- src/locales/locales.faq.js
+++ src/locales/locales.faq.js
@@ -66,5 +66,60 @@ const qa = [
             "en": "The rental and test period may vary from contract to contract and may be discussed and determined individually in each case. However, usually the contract has a minimum term of three months and the test period varies from 2 to 4 weeks."
         }
     },
+    {
+        "id": 7,
+        "question": {
+            "de": "Was ist die Servicegebühr von Großjungig?",
+            "en": "What is the Großjungig service fee?"
+        },
+        "answer": {
+            "de": 'Für unsere Dienste, z.B. die Nutzung der Plattform, Kundenbetreuung oder Konfliktvermittlung, erheben wir eine Servicegebühr, sobald ein Mietvertrag zwischen zwei Parteien (einer älteren und einer jüngeren Person) geschlossen wurde. ' +
+                `\n\n Wir haben zwei verschiedene Servicegebühren, eine für jede Kundengruppe. ` +
+                "\n 1.	Servicegebühr für Senior*innen " +
+                "\n Wir berechnen eine einmalige Gebühr von 50€, sobald Sie einen Mietvertrag mit eine*m neuen Mitbewohner*in abschließen. Sollten Sie innerhalb von 2 Monaten niemanden finden, müssen Sie nichts bezahlen. " +
+                "\n2. Servicegebühr für jüngere Personen  " +
+                "\nUnsere jungen Kunden zahlen eine Monatsgebühr von 10%. Die Gebühr berechnet sich anhand der monatlichen Warmmiete exkl. Steuern. Sie wird zu Beginn der Mietdauer berechnet (wenn Sie z.B. einen sechsmonatigen Mietvertrag ab dem 1. Juni 2021 haben, zahlen Sie sechs Monate lang jeweils am ersten des Monats). " +
+                "\nMehrwertsteuer auf Servicegebühren" +
+                "\nAbhängig von den jeweiligen Gesetzen kann auf die Servicegebühr Mehrwertsteuer erhoben werden. In diesem Fall wird die Servicegebühr die Mehrwertsteuer beinhalten.",
+
+            "en": "For our services, for example, platform use, customer support and conflict mediation, we change a service fee when a rental agreement between two parties (a senior and a young person) is confirmed." +
+                `\n\n We have two different service fees for each group of customers. ` +
+                "\n 1.	Service fee for senior clients " +
+                "\n We charge a one-time service fee of €50 after you sign a rental agreement with a new flat mate. We don’t charge this fee if you don’t find the right flatmate in two months." +
+                "\n2. Service fee for young clients " +
+                "\nOur young clients pay a monthly fee of 10%. The fee is calculated from a monthly “warm” rent (rent including utilities) but excluding taxes. This is charged at the beginning of rental period (for example, if you got a place for 6 months starting on June 1, 2021, you will be paying on the 1st of every month for the next six months)." +
+                "\nVAT charges on service fees" +
+                "\nDepending on the laws of the jurisdiction involved, VAT may be charged on top of the service fee. The service fee will include these VAT charges when applicable."
+        }
+    },
+    {
+        "id": 8,
+        "question": {
+            "de": "Ist die Großjungig-Servicegebühr erstattbar?",
+            "en": "Is the Großjungig service fee refundable? "
+        },
+        "answer": {
+            "de": "Wir berechnen keine Gebühren, bis Sie eine*n Mitbewohner*in gefunden haben. Sobald Sie einen Mietvertrag mit einer*m neuen Mitbewohner*in unterschreiben, gilt unser Service als erfüllt. Ab diesem Moment ist die Gebühr nicht erstattbar. " +
+            `\n\n Wenn Sie eine ältere Person sind und eine*n neuen Mieter*in finden wollen, müssen Sie diese Gebühr jeweils noch einmal zahlen. ` +
+            "\n\n Kunden von Großjungig ist es nicht gestattet, einen Vertrag miteinander ohne das Wissen von Großjungig abzuschließen. In solch einem Fall wird mit sofortiger Wirkung der weitere Zugang zu unseren Diensten verwehrt.",
+
+            "en": "We don’t change the Großjungig fee until you find a suitable flatmate. After you sign a rental agreement with a new flatmate, our matchmaking service is considered completed. The fee is then non-refundable." +
+                `\n\n If you are a senior client and would like to find a new tenant, you need to pay this fee again` +
+                "\n\n Großjungig users are not allowed to sign the renewed contract with each other without notifying Großjungig. In such an event the use of service will be denied indefinitely and no services of Großjungig will be provided effective immediately."
+
+        }
+    },
+    {
+        "id": 9,
+        "question": {
+            "de": "Wie geht Großjungig mit Mietkautionen um?",
+            "en": "How does Großjungig handle security deposit? "
+        },
+        "answer": {
+            "de": "Nur Vermieter*innen (Senior*innen) können eine Kaution verlangen. Manche Vermieter*innen verlangen eine Kaution für ihr Inserat. Die Höhe wird von ihnen festgelegt, nicht von Großjungig.  Wenn Sie eine junge Person sind und sich für ein Inserat mit einer Kaution interessieren, werden Sie über den Betrag informiert. Der Betrag und die Bezahlung der Kaution können von der*dem Senior*in und der jüngeren Person individuell festgelegt werden. Großjungig ist keine Partei in den Mietverträgen zwischen Senior*innen und jüngeren Personen. ",
+            "en": "Security deposit can only be required by hosts (seniors). Some hosts require a security deposit for their listing. The amount is set by the host, not by Großjungig. If you’re a young person and you’re interested in a listing with a security deposit, you’ll be informed about this amount. The amount and payment of a security deposit can be determined by a senior and a young person individually. Großjungig is not a party to the rental agreements between seniors and young persons."
+        }
+    }
+
 ]
 export default qa;
