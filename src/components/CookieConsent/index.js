c0678e2d1fa0f39a9bb4f64beb1f120966e6c670
bipul2002star <bipul.gautam@outlook.com>
2021-06-10 22:10:33 +0530

    Cookie Consent

--- src/components/CookieConsent/index.js
+++ src/components/CookieConsent/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import './index.css';
+import { NavLink } from "react-router-dom";
+import styles from'./index.module.css';
 import locales from '../../locales/locales.cookieconsent.json';
 
 const CookieConsent = (props) => {
   const lang = localStorage.getItem("lang");
   return (
-      <div className="cookie-consent">
-        <p className="cookie-consent__text">{locales.content[lang]}</p>
-        <button className="cookie-consent__btn" onClick={props.clicked}>
-          {locales.btnText[lang]}
-        </button>
+      <div className={styles["cc__body"]}>
+        <div className={styles["cc__content"]}>
+          <p className={styles["cc__title"]}>{locales.title[lang]}</p>
+          <p className={styles["cc__text"]}>{locales.content[lang]}</p>
+          <p className={styles["cc__desc"]}>{locales.desc[lang]}  <span> <NavLink to="/"> {locales.cookie[lang]} </NavLink> {lang === "en" ? <span> and </span> : <span> und </span>} <NavLink to='/termsandconditions'>  {locales.privacy[lang]} </NavLink> </span>.</p>
+        </div>
+        <div className={styles["cc__buttons"]}>
+            <button className={styles["whitecc__button"]} onClick={props.clicked}>
+              {locales.btnTextDismiss[lang]}
+            </button>
+
+            <button className={styles["bluecc__button"]} onClick={props.clicked}>
+              {locales.btnTextAccept[lang]}
+            </button>
+        </div>
       </div>
   )
 };
