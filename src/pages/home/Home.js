71549419b01b83871c3e910cdf5ba2fa9e183613
Nidanur Gunay <nidanur@sabanciuniv.edu>
2021-06-25 00:00:46 +0300

    Code redundancy eliminated

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -8,52 +8,64 @@ import { Card3 } from "../../components/styled";
 import Hero from "../home/components/Hero/Hero";
 import Why from "./components/Why/Why.js";
 import NewsLetter from "./components/NewsLetter/Newsletter.js";
-import {SecPanel, Mission} from "./styled";
+import { SecPanel, Mission } from "./styled";
 import { card3 } from "./cards";
 
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      test: false,
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: false,
+        };
+    }
 
-  componentDidMount() {
-    setTimeout(() => { this.setState({ test: !this.state.test }) }, 1000);
-  }
-  myCallback = (dataFromChild) => {
-    this.setState({test:dataFromChild})
-}
-  render() {
-    const lang = localStorage.getItem("lang");
-    return (
-      <>
+    componentDidMount() {
+        setTimeout(() => { this.setState({ time: !this.state.time }) }, 1000);
+    }
+    myCallback = (dataFromChild) => {
+        this.setState({ time: dataFromChild })
+    }
+    render() {
+        const lang = localStorage.getItem("lang");
+        return ( <
+            >
 
-      {this.state.test===true ?
-      
-      
-   <NewsLetter lang={lang} callbackFromParent={this.myCallback}></NewsLetter>
-      
-      
-      
-      :null}
+            {
+                this.state.time === true ?
 
-        <Hero/>
-        <Mission id="about">
-          <H3 className="home-mission">
-            {homeLocales.mission_1[lang]} <br /> {homeLocales.mission_2[lang]}
-          </H3>
-          <Link className="mission-button" to="/about">Learn more</Link>
-        </Mission>
-        <Why />
-        <SecPanel>
-          <Card3 text={homeLocales.opinion[lang]} source={card3[0].source} />
-        </SecPanel>
-      </>
-    );
-  }
+
+                <
+                NewsLetter lang = { lang }
+                callbackFromParent = { this.myCallback } > < /NewsLetter>
+
+
+
+                :
+                    null
+            }
+
+            <
+            Hero / >
+            <
+            Mission id = "about" >
+            <
+            H3 className = "home-mission" > { homeLocales.mission_1[lang] } < br / > { homeLocales.mission_2[lang] } <
+            /H3> <
+            Link className = "mission-button"
+            to = "/about" > Learn more < /Link> <
+            /Mission> <
+            Why / >
+            <
+            SecPanel >
+            <
+            Card3 text = { homeLocales.opinion[lang] }
+            source = { card3[0].source }
+            /> <
+            /SecPanel> <
+            />
+        );
+    }
 }
 
 export default Home;
